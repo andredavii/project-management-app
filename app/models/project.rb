9fcbb9ad20905ce2c980b01a65a6905a8846667a
Andre Davidian <davidianaa@masters.edu>
2023-11-29 04:02:07 +0000

    added percentage of completed tasks in dashboard model and table;

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,28 @@
 class Project < ApplicationRecord
   has_many :tasks
+
+  #total tasks that have complete == true
+  def complete_tasks
+    complete = tasks.select do |task|
+      task.complete?
+    end
+    complete.count
+  end
+
+  def total_tasks
+    tasks.count
+  end
+
+  #take percentage of the total amount and completed amount of tasks 
+  def completion
+    complete_tasks.to_f/total_tasks.to_f
+  end
+
+  def percentage
+    if tasks.present?
+       (completion*100).to_i
+    else 
+      0
+    end
+  end
 end
